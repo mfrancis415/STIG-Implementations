7df187bc8439c44f173c4ef8b89c904abcec4ea6
Monica Francis <mfrancis415@gmail.com>
2025-10-27 21:12:19 -0400

    Create PowerShell script to set DC power settings
    This script sets the DC power setting policy in the registry.

---
+++ WN10-CC-000145.ps1
@@ -0,0 +1,42 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures the DC (battery) power setting policy is enabled by setting DCSettingIndex to 1 in the Power Settings registry key.
+
+.NOTES
+    Author          : Monica Francis
+    LinkedIn        : linkedin.com/in/mfrancis415/
+    GitHub          : github.com/mfrancis415
+    Date Created    : 10-26-2025
+    Last Modified   : 10-26-2025
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000145
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
+
+# Define the registry path and value
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Power\PowerSettings\0e796bdb-100d-47d6-a2d5-f7d2daa51f51"
+$valueName = "DCSettingIndex"
+$valueData = 1  # DWORD 1
+
+# Ensure the registry key exists
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+# Set the DWORD value
+New-ItemProperty -Path $regPath -Name $valueName -Value $valueData -PropertyType DWord -Force | Out-Null
+
+# Confirm the change
+Get-ItemProperty -Path $regPath -Name $valueName
