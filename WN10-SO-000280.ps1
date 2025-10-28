115ab392d3886f93ce31e2fcbfad138d48e380dd
Monica Francis <mfrancis415@gmail.com>
2025-10-27 21:17:02 -0400

    Add PowerShell script for local password 60 day change

---
+++ WN10-SO-000280.ps1
@@ -0,0 +1,26 @@
+<#
+.SYNOPSIS
+    This PowerShell script configures the Account Lockout Duration policy to 15 minutes, allowing 0 as a valid alternative (meaning admin unlock required).
+
+.NOTES
+    Author          : Monica Francis
+    LinkedIn        : linkedin.com/in/mfrancis415/
+    GitHub          : github.com/mfrancis415
+    Date Created    : 10-26-2025
+    Last Modified   : 10-26-2025
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AC-000005
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\__remediation_template(STIG-ID-WN10-AU-000500).ps1 
+#>
